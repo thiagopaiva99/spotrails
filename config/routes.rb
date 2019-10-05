b9676d6a9eb7d5b94ab8ff949c9bcfe18f999cd3
Thiago Galvani <thiagopaiva99@gmail.com>
2019-10-05 12:35:50 -0300

    Mapping the correct routes to the searches

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'search/index'
-  get 'search/new'
   devise_for :users
   
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
+    resources :search, only: [:index, :new], as: :searches
   end
 
   unauthenticated :user do
